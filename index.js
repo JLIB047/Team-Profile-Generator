e53f2a334ee8592717f5df43daa4b30cc91b7e6a
Jason Libertelli <jason.libertelli@gmail.com>
2021-12-03 14:08:53 -0500

    prompt/write works

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+//link to generateHTML
+
+
+//node modules 
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+//team profiles 
 const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const { resolve } = require('path');
 
+//team array
 const teamArray =[];
 
 const addManager = () => {
@@ -47,6 +56,7 @@ const addManager = () => {
             }
         },
     ])
+    // organize data for manager 
     .then(managerInput => {
         const { name, id, email } = managerInput;
         const manager = new Manager (name, id, email);
@@ -56,4 +66,146 @@ const addManager = () => {
     })
 };
 
-addManager();
+
+const addEmployee = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: "Please choose your employee's role.",
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: "What's the name of the employee?",
+            validate: nameInput =>{
+                if(nameInput){
+                    return true;
+                } else {
+                    console.log('Please enter an employee name');
+                    return false;
+                }
+            }
+        }, 
+        {
+            type: 'input',
+            name: 'id',
+            message: "Please enter employee's Id.",
+            validate: idInput => {
+                if(idInput){
+                    return true;
+                } else {
+                    console.log("Please enter the employee's ID.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Please enter employee's Email.",
+            validate: emailInput => {
+                if(emailInput){
+                    return true;
+                } else {
+                    console.log("Please enter employee's email.");
+                    return false;
+                }
+            }
+        }, 
+        {
+            type: 'input', 
+            name: 'github',
+            message: "Please enter employee's github username.",
+            when: (input) => input.role === 'Engineer', 
+            validate: gitInput => {
+                if (gitInput){
+                    return true;
+                } else {
+                    console.log("Please enter a github username.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input', 
+            name: 'school',
+            message: "Please enter employee's School.",
+            when: (input) => input.role === 'Intern',
+            validate: schoolInput => {
+                if (schoolInput){
+                    return true;
+                } else {
+                    console.log("Please enter a school name.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            confrim: 'confirmAddEmployee',
+            message: 'Would you like to add more team members?', 
+            default: false
+        },
+   ])
+   .then(employeeData => {
+        let { name, id, email, role, github, school, confirmAddEmployee } = employeeData;
+        let employee;
+
+        // organize data for employee types 
+
+        if(role === 'Engineer') {
+            employee = new Engineer (name, id, email, github);
+
+            console.log(employee);
+        } else if (role === 'Intern') {
+            employee = new Intern (name, id, email, school);
+
+            console.log(employee)
+        }
+
+        teamArray.push(employee);
+
+        if(confirmAddEmployee){
+            return addEmployee(teamArray);
+        } else {
+            return teamArray;
+        }
+   })
+
+};
+
+//function to generate HTML page using file system 
+const writeFile = data => {
+    fs.writeFile('./dist/index.html', data, err => {
+        if(err) {
+            console.log(err);
+            return;
+        } else {
+            console.log("Your team profile has been successfully created! You can view it in Index.html")
+        }
+    })
+};
+
+addManager()
+    .then(addEmployee)
+    .then(teamArray => {
+        return generateHTML(teamArray);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+    
+
+
+
+
+
+
+
+
+    
